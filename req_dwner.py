99adcb0fb6e72ace2336704cc94498eb005e9845
Constantine Dev <constantinedev@protonmail.com>
2024-08-04 11:07:19 +0100

    Update req_dwner.py

--- req_dwner.py
+++ req_dwner.py
@@ -14,21 +14,21 @@ session.proxies['http'] = 'socks5h://localhost:9050'
 session.proxies['https'] = 'socks5h://localhost:9050'
 
 async def dwner(dwn_url, dwn_path, methods):
-	filename = os.path.basename(urlparse(dwn_url).path)
-	output_path = os.path.expanduser(dwn_path + filename)
-	os.makedirs(os.path.dirname(output_path), exist_ok=True)
-	if methods == "tor":
-		response = session.get(dwn_url, stream=True)
-	elif methods == "nor":
-		response = requests.get(dwn_url, stream=True)
-	total_size = int(response.headers.get('content-length', 0))
-	block_size = 1024
-	
-	tqdm_bar = tqdm(total=total_size, unit="%", unit_scale=True, desc="Downloading")
-	with open(dwn_path+filename, 'wb') as file:
-		for data in response.iter_content(block_size):
-			if data:
-				tqdm_bar.update(len(data))
-				file.write(data)
-	tqdm_bar.close()
-	print(f'Download Complete => {dwn_path + filename}')
+  filename = os.path.basename(urlparse(dwn_url).path)
+  output_path = os.path.expanduser(dwn_path + filename)
+  os.makedirs(os.path.dirname(output_path), exist_ok=True)
+  if methods == "tor":
+    response = session.get(dwn_url, stream=True)
+  elif methods == "nor":
+    response = requests.get(dwn_url, stream=True)
+  total_size = int(response.headers.get('content-length', 0))
+  block_size = 1024
+
+  tqdm_bar = tqdm(total=total_size, unit="%", unit_scale=True, desc="Downloading")
+  with open(dwn_path+filename, 'wb') as file:
+    for data in response.iter_content(block_size):
+      if data:
+        tqdm_bar.update(len(data))
+        file.write(data)
+  tqdm_bar.close()
+  print(f'Download Complete => {dwn_path + filename}')
