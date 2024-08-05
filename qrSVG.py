72c263e4b9ef0027003abd0a46195a8e4aea3739
Constantine Dev <constantinedev@protonmail.com>
2024-08-05 11:44:58 +0100

    Update qrSVG.py

--- qrSVG.py
+++ qrSVG.py
@@ -1,16 +1,25 @@
 import re, io, os, sys, ast, json, sqlite_utils, asyncio
 from datetime import datetime as DT, timezone as TZ, timedelta as TD
-import qrcode
+import qrcode, qrcode.constants
 from qrcode.image.svg import SvgPathFillImage
 
 async def qr_svg(data, classname, box_size):
   if box_size == "" or box_size is None:
     box_size = 20
+  
+  qr = qrcode.QRCode(
+    version=5, 
+    box_size=int(box_size), 
+    border=0, 
+    error_correction=qrcode.constants.ERROR_CORRECT_L, 
+    image_factory=SvgPathFillImage
+  )
+  qr.add_data(data)
+
   if classname == "" or classname is None:
     classname = "qr_image"
-  qr = qrcode.QRCode(image_factory=SvgPathFillImage, box_size=int(box_size))
-  qr.add_data(data)
-  img = qr.make_image(attrib={'class': classname, "name": classname})
+	
+  img = qr.make_image(attrib={'class': classname})
   svg = img.to_string(encoding="unicode")
   print(svg)
   return svg
