61b1b3ef40be831f5d1b1f1d1040ef56439aab29
Constantine Dev <constantinedev@protonmail.com>
2024-11-29 02:27:55 +0000

    Update aiohttpRequest.py

--- aiohttpRequest.py
+++ aiohttpRequest.py
@@ -1,6 +1,5 @@
-import re, io, os, sys, ast, ssl, csv, json, sqlite_utils, requests, asyncio, base64
-from datetime import datetime as DT, timezone as TZ, timedelta as TD
-import cloudscraper
+import re, os, io, sys, ast, ssl, csv, json, sqlite_utils, asyncio, aiohttp, base64
+from aiohttp_socks import ProxyType,ProxyConnector, ChainProxyConnector
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem, HardwareType, SoftwareEngine
 
@@ -11,100 +10,66 @@ software_engines = [SoftwareEngine.GECKO.value, SoftwareEngine.WEBKIT.value, Sof
 user_agent_rotator = UserAgent(software_names=software_names, software_engines=software_engines, hardware_types=hardware_types, operating_systems=operating_systems, limit=100)
 user_agent = user_agent_rotator.get_random_user_agent()
 
-session = requests.session()
-session.proxies = {}
-session.proxies['http'] = 'socks5h://localhost:9050'
-session.proxies['https'] = 'socks5h://localhost:9050'
-
-headeres = {
-	'User-Agent': user_agent,
-	'Content-Type': 'text/html',
-}
-
 async def gun_shell(url, method, type, headers, payload):
   if url == "" or url is None:
-    return {"status": "Error", "response": "URL was empty."}
-  
+    return {"status": "Error", "response": "URL was empty!"	}
   if headers=={} or headers is None:
-    headers.updates({'user-Agent':user_agent,'Content-Type': 'html/text'})
-  else:
-    headers.update(headers)
-  
+    headers={"User-Agent":user_agent, "Content-Type": 'application/json'}
   if payload=={} or payload is None:
-    payload = {}
-  if type == "nor":
-    return await nor_req(url, method, type, headers, payload)
-  if type == "tor":
-    return await tor_req(url, method, type, headers, payload)
-  
-async def nor_req(url, method, type, headers, payload):
-	if method == "GET":
-		req = requests.get(url, headers=headers, data=payload)
-		if req.status_code == 200:
-			retData = {
-				'status': req.status_code,
-				'method': method,
-				'type': type,
-				'response': req.content
-      }
-			return retData
-		else:
-			Cloudscraper = cloudscraper.create_scraper(sess=headers)
-			result = Cloudscraper.get(url, headers=headers)
-			if result.status_code == 200:
-				retData = {
-					'status': result.status_code,
-					'method': method,
-					'type': type,
-					'response': result.content
-				}
-				return retData
-			else:
-				retData = {
-					'status': result.status_code,
-					'method': method,
-					'type': type,
-					'response': ""
-				}
-				return retData
+    payload={}
+  elif payload is not None:
+    try:
+      payload = ast.literal_eval(payload)
+    except:
+      payload = json.loads(payload)
 
-	elif method == "POST":
-		req = requests.post(url, headers=headers, data=payload)
-	if req.status_code == 200:
-		retData = {
-			"status": req.status_code,
-			'method': method,
-			'type': type,
-			"response": req.content
-		}
-		return retData
-	else:
-		retData = {
-			"status": req.status_code,
-			'method': method,
-			'type': type,
-			'response': ""
-		}
-		return retData
+  if type=="nor":
+    async with aiohttp.ClientSession() as session:
+      return await bulletfire(session, url, method, type, headers, payload)
+  elif type=="tor":
+    connector = ProxyConnector.from_url("socks5://127.0.0.1:9050")
+    async with aiohttp.ClientSession(connector=connector) as session:
+      return await bulletfire(session, url, method, type, headers, payload)
 
-async def tor_req(url, method, type, headers, payload):
-	if method == "GET":
-		req = session.get(url, headers=headers, data=payload)
-	elif method == "POST":
-		req = session.post(url, headers=headers, data=payload)
-	if req.status_code == 200:
-		retData = {
-			"status": req.status_code,
-			'method': method,
-			'type': type,
-			"response": req.text
-		}
-		return retData
-	else:
-		retData = {
-			"status": req.status_code,
-			'method': method,
-			'type': type,
-			'response': ""
-		}
-		return retData
+async def bulletfire(session, url, method, type, headers, payload):
+  timeout = aiohttp.ClientTimeout(total=5)
+  if method == "GET":
+    if headers == "" or headers is None:
+      headers = {'user-agent': user_agent, 'Contnet-Tpye': 'text/html'}
+    async with session.get(url, headers=headers, data=payload, ssl=ssl.SSLContext()) as response:
+      if response.status == 200:
+        retData = {
+          'status': response.status,
+          'method': method,
+          'type': type,
+          'response': await response.text()
+        }
+        return retData
+      else:
+        retData = {
+          'status': response.status,
+          'method': method,
+          'type': type,
+          'response': ""
+        }
+        return retData
+  elif method == "POST":
+    if headers == "" or headers is None:
+      headers = {'user-agent': user_agent, 'Contnet-Tpye': 'application/josn'}
+    async with session.post(url, headers=headers, data=payload, ssl=ssl.SSLContext()) as response:
+      if response.status == 200:
+        retData = {
+          'status': response.status,
+          'method': method,
+          'type': type,
+          'response': json.loads(await response.text())
+        }
+        return retData
+      else:
+        retData = {
+          'status': response.status,
+          'method': method,
+          'type': type,
+          'response': ""
+        }
+        return retData
