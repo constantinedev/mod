51d7110f674b3bb423c5e08f137b24f37c8d85d1
Constantine Dev <constantinedev@protonmail.com>
2024-11-29 02:28:32 +0000

    Update asyncrequests.py

--- asyncrequests.py
+++ asyncrequests.py
@@ -1,9 +1,9 @@
-import re, io, os, sys, ast, json, sqlite_utils, requests, asyncio
+import re, io, os, sys, ast, ssl, csv, json, sqlite_utils, requests, asyncio, base64
 from datetime import datetime as DT, timezone as TZ, timedelta as TD
-from aiohttp_socks import ProxyType, ProxyConnector, ProxyConnectionError
-
+import cloudscraper
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem, HardwareType, SoftwareEngine
+
 operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.UNIX.value, OperatingSystem.MAC.value]
 hardware_types = [HardwareType.MOBILE.value, HardwareType.SERVER.value]
 software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.SAFARI.value, SoftwareName.ANDROID.value]
@@ -13,65 +13,98 @@ user_agent = user_agent_rotator.get_random_user_agent()
 
 session = requests.session()
 session.proxies = {}
-session.proxies['http'] = 'socks5h://127.0.0.1:9050'
-session.proxies['https'] = 'socks5h://127.0.0.1:9050'
+session.proxies['http'] = 'socks5h://localhost:9050'
+session.proxies['https'] = 'socks5h://localhost:9050'
 
-# headers = {'User-Agent': user_agent}
-# payload = {}
+headeres = {
+	'User-Agent': user_agent,
+	'Content-Type': 'text/html',
+}
 
 async def gun_shell(url, method, type, headers, payload):
   if url == "" or url is None:
-    return {"status":"Error","response":"URL was empty!"}
+    return {"status": "Error", "response": "URL was empty."}
+  
   if headers=={} or headers is None:
-    headers = {'User-Agent': user_agent}
+    headers.updates({'user-Agent':user_agent,'Content-Type': 'html/text'})
+  else:
+    headers.update(headers)
+  
   if payload=={} or payload is None:
-    payload={}
-
+    payload = {}
   if type == "nor":
     return await nor_req(url, method, type, headers, payload)
   if type == "tor":
     return await tor_req(url, method, type, headers, payload)
-
+  
 async def nor_req(url, method, type, headers, payload):
-  if method == "GET":
-    req = requests.get(url, headers=headers, data=payload)
-  elif method == "POST":
-    req = requests.post(url, headers=headers, data=payload)
-  if req.status_code == 200:
-    retData = {
-      "status": req.status_code,
-      'method': method,
-      'type': type,
-      "response": req.text
-    }
-    return retData
-  else:
-    retData = {
-      "status": req.status_code,
-      'method': method,
-      'type': type,
-      'response': ""
-    }
-    return retData
+	if method == "GET":
+		req = requests.get(url, headers=headers, data=payload)
+		if req.status_code == 200:
+			retData = {
+				'status': req.status_code,
+				'method': method,
+				'type': type,
+				'response': req.content
+      }
+			return retData
+		else:
+			Cloudscraper = cloudscraper.create_scraper(sess=headers)
+			result = Cloudscraper.get(url, headers=headers)
+			if result.status_code == 200:
+				retData = {
+					'status': result.status_code,
+					'method': method,
+					'type': type,
+					'response': result.content
+				}
+				return retData
+			else:
+				retData = {
+					'status': result.status_code,
+					'method': method,
+					'type': type,
+					'response': ""
+				}
+				return retData
+
+	elif method == "POST":
+		req = requests.post(url, headers=headers, data=payload)
+	if req.status_code == 200:
+		retData = {
+			"status": req.status_code,
+			'method': method,
+			'type': type,
+			"response": req.content
+		}
+		return retData
+	else:
+		retData = {
+			"status": req.status_code,
+			'method': method,
+			'type': type,
+			'response': ""
+		}
+		return retData
 
 async def tor_req(url, method, type, headers, payload):
-  if method == "GET":
-    req = session.get(url, headers=headers, data=payload)
-  elif method == "POST":
-    req = session.post(url, headers=headers, data=payload)
-  if req.status_code == 200:
-    retData = {
-      "status": req.status_code,
-      'method': method,
-      'type': type,
-      "response": req.text
-    }
-    return retData
-  else:
-    retData = {
-      "status": req.status_code,
-      'method': method,
-      'type': type,
-      'response': ""
-    }
-    return retData
+	if method == "GET":
+		req = session.get(url, headers=headers, data=payload)
+	elif method == "POST":
+		req = session.post(url, headers=headers, data=payload)
+	if req.status_code == 200:
+		retData = {
+			"status": req.status_code,
+			'method': method,
+			'type': type,
+			"response": req.text
+		}
+		return retData
+	else:
+		retData = {
+			"status": req.status_code,
+			'method': method,
+			'type': type,
+			'response': ""
+		}
+		return retData
