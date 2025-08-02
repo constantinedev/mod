9c66e37072d5dc792f8513e8c1f6161fe6b9e3d8
Constantine Dev <constantinedev@protonmail.com>
2025-08-02 07:30:57 +0100

    Update callocv.py

--- callocv.py
+++ callocv.py
@@ -1,21 +1,14 @@
-import re, os, io, sys, ast, ssl, json, csv, sqlite_utils, requests, asyncio, time, pgpy
+import re, io, os, sys, ast, ssl, base64, json, csv, sqlite_utils, asyncio, requests
 from datetime import datetime as DT, timezone as TZ, timedelta as TD, time as TIME, date
 from sqlite_utils.utils import sqlite3
 
-import cv2, vlc, threading, yt_dlp, imutils, qrcode, pyotp, jwt
+import cv2, vlc, yt_dlp, threading, time, imutils
+import numpy as np
 
-object_detector = cv2.createBackgroundSubtractorKNN()
 qrcode_detector = cv2.QRCodeDetector()
-face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-plates_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_license_plate_rus_16stages.xml')
-
-### Play Audio
-def play_audio(url):
-	if not url:
-		return
-	player = vlc.MediaPlayer(url)
-	player.play()
-	return player
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=True)
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+plates_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_license_plate_rus_16stages.xml")
 
 ### YouTube Stream URL
 def ytStream(url, cookies_path=None):
@@ -40,20 +33,35 @@ def ytStream(url, cookies_path=None):
 		yt_fps = info.get('fps', 30)
 		return video_url, audio_url, yt_fps
 
-async def snapshort(frame, fileName, filePath : None):
+def snapshort(frame, filePath: None, fileName):
 	if not filePath:
-		file_save_as = f"temps/images/cvcap/{fileName}.jpg"
+		file_save_as = f"{fileName}.jpg"
 	else:
+		if not os.path.exists(filePath):
+			os.makedirs(filePath)
 		file_save_as = f"{filePath}/{fileName}.jpg"
 	cv2.imwrite(file_save_as, frame)
-	return
 
-### Config Cmaner
+def play_audio(url):
+	if not url:
+		return
+	player = vlc.MediaPlayer(url)
+	player.play()
+	return player
+
 def callcamera(resources):
 	cap = cv2.VideoCapture()
+	if cv2.cuda.getCudaEnabledDeviceCount() > 0:
+		cv2.cuda.setDevice(0)
+		print(f"CUDA Detected Devices: {cv2.cuda.getCudaEnabledDeviceCount()}")
+		CUDA_STATUS = True
+	else:
+		print(f"CUDA NOT Detected Devices: {cv2.cuda.getCudaEnabledDeviceCount()}")
+		CUDA_STATUS = False
+	
 	match resources:
 		case int():
-			cap.open(int(resources), cv2.CAP_DSHOW)
+			cap.open(int(resources))
 			cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 			cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 		case str():
@@ -71,100 +79,55 @@ def callcamera(resources):
 			exit()
 	return cap
 
-CUDA_STATUS = False
-### OpenCV Core
-def streaming(camera_src, brightness_gain, label_border, min_area, max_area):
+def corecv(cap, brightness_gain, label_border, min_area, max_area):
+	ref, frame = cap.read()
+	width, height, fps = frame.shape
+	if not ref:
+			if cap : cap.release()
+			print("Failed to grab frame!")
+			return None
 	if cv2.cuda.getCudaEnabledDeviceCount() > 0:
-		cv2.cuda.setDevice(0)
-		CUDA_STATUS = True
-		print(f"CUDA Device(s) Detected! {cv2.cuda.getCudaEnabledDeviceCount()}")
-	else:
-		CUDA_STATUS = False
-		print(f"CUDA Device(s) Not Detected! {cv2.cuda.getCudaEnabledDeviceCount()}, CPU are running...")
-	
-	cap = callcamera(camera_src)
-	
-	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-	height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-	fps = int(cap.get(cv2.CAP_PROP_FPS))
-	counts = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-	print(width, height, fps, counts)
-	
-	fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-	temp_video_path = 'temp_video.mp4'
-	out = cv2.VideoWriter(temp_video_path, fourcc, fps, (width, height))
-	
-	if not cap.isOpened():
-		if cap : cap.release()
-		if out : out.release()
-		print(f"Camera not Connected!")
-	
-	while True:
-		frame, mask = ocvcore(cap, brightness_gain, label_border, min_area, max_area)
-		if frame is None:
-			break
-
-		### DEBUG FOR SHOWING TEH FRAME
-		cv2.imshow("FRAME SHOW", frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-	cap.release()
-	cv2.destroyAllWindows()
-### OpenCV Core [END]
-
-def ocvcore(cap, brightness_gain, label_border, min_area, max_area):
-	ret, frame = cap.read()
-	# width, height, fps = cap.shape
-	if not ret:
-		return None, None
-
-	if brightness_gain is None:
-		brightness_gain = 1.0
-	else:
-		brightness_gain = int(brightness_gain)
-	bright_tuner = cv2.convertScaleAbs(frame, alpha=brightness_gain, beta=0)
-
-	if CUDA_STATUS:
+		cv2.ocl.setUseOpenCL(True)
 		gpu_frame = cv2.cuda_GpuMat()
-		gpu_frame.upload(bright_tuner)
+		gpu_frame.upload(frame)
 		cuda_frame = gpu_frame.download()
-	else: pass
-	
-	frame = cuda_frame if CUDA_STATUS else bright_tuner
-	mask = object_detector.apply(frame)
-	gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	
-	qrcodeDetect(frame)
-	motionDetection(frame, mask, label_border, min_area, max_area)
-	faceDetection(frame, gray_image, label_border)
-	wordsDecetion(frame, gray_image)
-	
-	return frame, mask
+		CUDA_STATUS = True
+	else: 
+		cv2.ocl.setUseOpenCL(True)
+		CUDA_STATUS = False
+		
+	brightness_tunner = cv2.convertScaleAbs(cuda_frame if CUDA_STATUS else frame, alpha=1.0 if brightness_gain is None else int(brightness_gain), beta=20)
+	mask = object_detector.apply(brightness_tunner)
+	gray_image = cv2.cvtColor(brightness_tunner, cv2.COLOR_BGR2GRAY)
 
-### FACE DETECTION
+	qrcodeDetection(brightness_tunner if not CUDA_STATUS else cuda_frame)
+	motionDetection(brightness_tunner if not CUDA_STATUS else cuda_frame, mask, label_border, min_area, max_area)
+	faceDetection(brightness_tunner if not CUDA_STATUS else cuda_frame, gray_image, label_border)
+	wordsDetection(brightness_tunner if not CUDA_STATUS else cuda_frame, gray_image)
+
+	return brightness_tunner, mask, gray_image, CUDA_STATUS
+
+### Face Detection
 def faceDetection(frame, gray_image, label_border):
-	face = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50), flags=cv2.CASCADE_SCALE_IMAGE)
-
+	face = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=10, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+ 
 	if len(face) > 0:
 		x, y, w, h = face[0]
 		bbox = (x, y, h, w)
 		try:
 			tracker = cv2.legacy.TrackerCSRT_create()
 			tracker.init(frame, bbox)
-			
-			# timestamp = DT.now().strftime("%Y-%m-%d_%H-%M-%S")
-			timestamp = DT.now().strftime("%Y-%m-%d_%H-%M")
-			fileName = timestamp
-			# snapshort(frame, fileName, None)
 		except AttributeError:
 			print("Error: CSRT tracker not available. Ensure opencv-contrib-python is installed.")
 			return
-
+		# timestamp = DT.now().strftime("%Y-%m-%d_%H-%M-%S")
+		# snapshort(frame, filePath=None, fileName=timestamp)
+		# print(f"Saved snapshot: {timestamp}.jpg")
+ 
 	for (x, y, w, h) in face:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
 		cv2.putText(frame, "FACE DETECT", (x, y - int(label_border)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
 
-### MOTION DETECT
 def motionDetection(frame, mask, label_border, min_area, max_area):
 	contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	for contour in contours:
@@ -172,24 +135,21 @@ def motionDetection(frame, mask, label_border, min_area, max_area):
 		if int(min_area) < area < int(max_area):
 			(x, y, w, h) = cv2.boundingRect(contour)
 			cv2.rectangle(frame, (x - int(label_border), y - int(label_border)), (x + w + int(label_border), y + h + int(label_border)), (0, 255, 0), 1)
-			cv2.putText(frame, "DETECT", (x - int(label_border), y - int(label_border)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
-			# timestamp = DT.now().strftime("%Y-%m-%d_%H-%M")
-			# fileName = timestamp
-			# snapshort(frame, fileName, None)
-		# elif 1800 < area < 2000:
-		# 	(x, y, w, h) = cv2.boundingRect(contour)
-		# 	cv2.rectangle(frame, (x - int(label_border), y - int(label_border)), (x + w + int(label_border), y + h + int(label_border)), (0, 255, 255), 1)
-		# 	cv2.putText(frame, "DETECT", (x - int(label_border), y - int(label_border)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
+			cv2.putText(frame, "DETECT", (x- int(label_border), y - int(label_border)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+		elif 2000 < area < 2800:
+			(x, y, w, h) = cv2.boundingRect(contour)
+			cv2.rectangle(frame, (x - int(label_border), y - int(label_border)), (x + w + int(label_border), y + h + int(label_border)), (0, 255, 255), 1)
+			cv2.putText(frame, "DETECT", (x- int(label_border), y - int(label_border)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
 		else:
 			continue
 
-### QRCode Detect
-def qrcodeDetect(frame):
+### QR Code Detection
+def qrcodeDetection(frame):
 	data, points, _ = cv2.QRCodeDetector().detectAndDecode(frame)
 	if points is not None:
 		points = points[0]
 		points = [(int(point[0]), int(point[1])) for point in points]
-
+		
 		for i in range(len(points)):
 			pt1 = points[i]
 			pt2 = points[(i+1) % len(points)]
@@ -197,25 +157,82 @@ def qrcodeDetect(frame):
 		if data:
 			qr_content = data
 			print(qr_content)
-			# snapshort(frame, "testsave.jpg", None)
-			# return qr_content
-			### Make Output Data to NEXT step
 
-def wordsDecetion(frame, gray_image):
-	plates = plates_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(20, 20), flags=cv2.CASCADE_SCALE_IMAGE)
+### Words Detection
+def wordsDetection(frame, gray_image):
+	plates = plates_cascade.detectMultiScale(gray_image, scaleFactor=1.5, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 	for (x,y,w,h) in plates:
-		plates_rec = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 1)        
-		cv2.putText(plates_rec, 'Text', (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
+		plates_rec = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 1)        
+		cv2.putText(plates_rec, 'Text', (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1)
 
 		# fileName = f"{DT.now().strftime('%Y-%m-%d_%H-%M')}"
-		# snapshort(frame, fileName, None)
+		# snapshort(frame, None, fileName)
 
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(streaming())
+############################
+### You can copy the following code to use in your project
+### to reacall your cv from function
+### * StreamViewer : You need to follow the rules to give the values of function,
+### * cap : call camera from functoin, you can also <callcamera> from the other functoin, 
+### 				This is include the CUDA checking, and will return the Video Capture with src.
+### * corecv : Real OpenCV Frame filter, you must make the detection in the <corecv> function
+### ** The Demo are show in __main__, update the info for test your devices.
+############################
+############################COPY THIS CODE TO YOUR PROJECT############################>
+### MAIN Function
+def StreamViewer(camera_src, brightness_gain, label_border, min_area, max_area):
+	cap = callcamera(camera_src)
 
-### Python Version Request: up to 3.11
-### Modueles requests: opencv-python opencv-python-headless opencv-contrib-python opencv-contrib-python-headless 
-### Options requests: CUDA
-### Descriptions: craete your cap to ocvcore and get frame and mask with other detections. 
-### streaming: is also the debug when you given the drightness, boxs border, min aree, max area for showing with imshow() for debug.
-### If you want to use the modules with a copy you can edit in "streaming" for development your funciton.
+	if not cap.isOpened():
+		print(f"Failed to open camera source: {camera_src}")
+		exit()
+	
+	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+	height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+	fps = cap.get(cv2.CAP_PROP_FPS)
+	print(f"Camera Resolution: {width}x{height}, FPS: {fps}")
+	### SAVE STREAM to MP4
+	fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+	temp_video_path = 'temp_video.mp4'
+	out = cv2.VideoWriter(temp_video_path, fourcc, fps, (width, height))
+	
+	while True:
+		frame, mask, gray_image, CUDA_STATUS = corecv(cap, brightness_gain, label_border, min_area, max_area)
+		if frame is None:
+			print("Failed to grab frame!")
+			break
+	
+		# out.write(frame)  # Save the frame to the video file
+		MainFrameViewerName, MaskFrameViewerName = "VIEWER", "MASK"
+		cv2.namedWindow(f"{MainFrameViewerName} - (GPU)" if CUDA_STATUS else f"{MainFrameViewerName} - (CPU)", cv2.WINDOW_NORMAL)
+		cv2.imshow(f"{MainFrameViewerName} - (GPU)" if CUDA_STATUS else f"{MainFrameViewerName} - (CPU)", frame)
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	cap.release()
+	cv2.destroyAllWindows()
+############################COPY THIS CODE TO YOUR PROJECT############################>
+
+if __name__ == "__main__":
+	username, password, ip, channel = "cctv95", "Newland1", "192.168.0.17", 4
+	path = f"channel={int(channel)}&stream=0.sdp"
+	canera_src = f"rtsp://{username}:{password}@{ip}/{path}"
+	label_border, brightness_gain = 12, 1.0
+	min_area, max_area = 450, 900
+	StreamViewer(canera_src, brightness_gain, label_border, min_area, max_area)
+
+############################
+### callcamera : check CUDA and Canera Source, make it reture the VideoCapture object
+### corecv : Core of the OpenCV frame filter, Decetion must be in <corecv>
+### play_audio: Play audio file by Python-VLC
+### snapshort: Take a snapshot from the video stream (<frame>, <filePath>: Default=None, <fileName>[No need to put {.mp4} at the end.])
+### motionDetection: Detect motion in the video stream (<frame>, <brightness_gain>, <label_border>, <min_area>, <max_area>)
+### qrcodeDetection: Detect QR codes in the video stream (<frame>), "snapshot" = Disable Default
+### faceDetection: Detect faces in the video stream (<frame>) "snapshot" = Disable Default
+### wordsDetection: Detect words in the video stream
+### ==========================
+### object_detector : using createBackgroundSubtractorMOG2()
+### face_classifier : OpenCV face detection basic model. [haarcascade_frontalface_default.xml]
+### words_classifier : OpenCV text detection basic model. [haarcascade_russian_plate_number.xml]
+### CUDA_STATUS : Default False to disable, True to enable
+### python3 -m pip install -U opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless
+############################
